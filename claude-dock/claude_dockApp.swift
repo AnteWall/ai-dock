289d136efb3bac75c48a9836d54989047cc83365
Ante Wall <antewall@gmail.com>
2026-02-12 12:42:21 +0100

    Initial Commit

---
+++ claude-dock/claude_dockApp.swift
@@ -0,0 +1,17 @@
+//
+//  claude_dockApp.swift
+//  claude-dock
+//
+//  Created by Ante Wall on 2026-02-12.
+//
+
+import SwiftUI
+
+@main
+struct claude_dockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
